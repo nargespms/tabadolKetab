54db1a9930fb3d91b6663cb3b96247084b39bbc6
Mohsen Tohidipour <tohidipor@gmail.com>
2020-11-30 13:42:37 +0330

    add profile

---
+++ src/components/users/profileCmp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="d-flex flex-row-reverse ma-4">
+      <v-btn>{{ $t('changePassword') }}</v-btn>
+      <v-btn class="ml-4">
+        {{ $t('filteredFileDl') }}
+      </v-btn>
+    </div>
+    <div class="elevation-1 text-center ma-4">
+      asd
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'profilecmp',
+
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+.v-btn {
+  letter-spacing: inherit;
+}
+</style>
