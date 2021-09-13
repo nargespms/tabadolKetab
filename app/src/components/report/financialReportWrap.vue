db4af35bb89175f51da59109ca32c2a7f8abf16f
narges <nargespms.np@gmail.com>
2021-09-13 11:53:58 +0430

    add natioanl id to creat invoice from admin

--- app/src/components/report/financialReportWrap.vue
+++ app/src/components/report/financialReportWrap.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <!-- <userBuyReport /> -->
     <categoryBuyReport class="mt-6" />
     <cashierFinanceReport class="mt-6" />
     <booksTagReport class="mt-6" />
+    <userBuyReport class="mt-6" />
   </div>
 </template>
 
 <script>
-// import userBuyReport from './userBuyReport.vue';
+import userBuyReport from './userBuyReport.vue';
 import categoryBuyReport from './categoryBuyReport.vue';
 import cashierFinanceReport from './cashierFinanceReport.vue';
 import booksTagReport from './booksTagReport.vue';
@@ -16,7 +16,7 @@ import booksTagReport from './booksTagReport.vue';
 export default {
   name: 'financialReportWrap',
   components: {
-    // userBuyReport,
+    userBuyReport,
     categoryBuyReport,
     cashierFinanceReport,
     booksTagReport,
--- app/src/components/users/getClientByMobile.vue
+++ app/src/components/users/getClientByMobile.vue
@@ -3,27 +3,49 @@
     <div
       class="clientNumberWrap px-3 pt-3 d-flex align-center justify-space-between"
     >
-      <div class="w250">
-        <mobilePhone
-          @setMobilePhone="setMobilePhone"
-          :validate="true"
-          :mode="'edit'"
-          :isRequired="true"
-          :phone="false"
-          :key="mobileKey"
-        />
-      </div>
+      <v-tabs light vertical background-color="transparent" v-model="tab">
+        <v-tab
+          v-for="tab in tabs"
+          :key="tab.index"
+          class="justify-space-between"
+        >
+          <v-icon class="pl-4">
+            {{ tab.icon }}
+          </v-icon>
+          {{ $t(tab.text) }}
+        </v-tab>
+
+        <v-tab-item background-color="transparent" class="w250">
+          <div class="w250 pr-8">
+            <mobilePhone
+              @setMobilePhone="setMobilePhone"
+              :validate="true"
+              :mode="'edit'"
+              :isRequired="true"
+              :phone="false"
+              :key="mobileKey"
+            />
+          </div>
+        </v-tab-item>
+        <v-tab-item>
+          <div class="w250 pr-8">
+            <nationalId
+              @setNationalId="setNationalId"
+              :isRequire="true"
+              :key="nationalIdKey"
+            />
+          </div>
+        </v-tab-item>
+      </v-tabs>
       <div>
         <v-btn color="success" @click="getClientInfo" class="d-block mb-3">
-          <v-icon color="white" v-bind="attrs" v-on="on" class="pl-1">
+          <v-icon color="white" class="pl-1">
             mdi-account-check
           </v-icon>
           {{ $t('getClient') }}
         </v-btn>
         <v-btn color="primary" @click="addUserModal" class="d-block mb-3">
-          <v-icon color="white" v-bind="attrs" v-on="on" class="pl-2"
-            >mdi-account-plus
-          </v-icon>
+          <v-icon color="white" class="pl-2">mdi-account-plus </v-icon>
           {{ $t('AddUser') }}
         </v-btn>
       </div>
@@ -49,6 +71,8 @@
 
 <script>
 import mobilePhone from '../userControls/mobilePhone.vue';
+import nationalId from '../userControls/nationalId.vue';
+
 import AddUser from './addUser.vue';
 import moneyFormat from '../../mixins/moneyFormat.js';
 
@@ -57,36 +81,79 @@ export default {
   components: {
     mobilePhone,
     AddUser,
+    nationalId,
   },
   mixins: [moneyFormat],
 
   data() {
     return {
       mobile: '',
+      nationalId: '',
       client: {},
+
       mobileKey: 0,
+      nationalIdKey: 0,
+
       adduserModal: false,
+
+      tab: '',
+      tabs: [
+        { text: 'mobile', icon: 'mdi-card-account-phone-outline' },
+        { text: 'nationalId', icon: 'mdi-card-account-details-star-outline' },
+      ],
     };
   },
   methods: {
     setMobilePhone(value) {
       this.mobile = value;
-      console.log('ddd');
+    },
+    setNationalId(value) {
+      this.nationalId = value;
     },
     getClientInfo() {
-      console.log(this.mobile);
+      this.client = {};
+      if (this.tab === 0) {
+        this.getClientBymobile();
+      } else if (this.tab === 1) {
+        this.getClientByNationalId();
+      }
+    },
+
+    getClientBymobile() {
       this.$axios
         .get(`/v1/api/tabaadol-e-ketaab/client/mobile/${this.mobile}`)
         .then(res => {
           if (res.status === 200) {
-            console.log(res);
             this.$emit('setUser', res.data.user.id);
             this.client = res.data.user;
             this.mobile = '';
-            // this.mobileKey += 1;
+            this.nationalId = '';
+            this.nationalIdKey += 1;
           }
         })
         .catch(e => {
+          this.$emit('clientError');
+
+          if (e.response.status === 404) {
+            this.$emit('clientError');
+          }
+        });
+    },
+
+    getClientByNationalId() {
+      this.$axios
+        .get(`/v1/api/tabaadol-e-ketaab/client/nationalId/${this.nationalId}`)
+        .then(res => {
+          if (res.status === 200) {
+            this.$emit('setUser', res.data.user.id);
+            this.client = res.data.user;
+
+            this.mobileKey += 1;
+          }
+        })
+        .catch(e => {
+          this.$emit('clientError');
+
           if (e.response.status === 404) {
             this.$emit('clientError');
           }
@@ -110,5 +177,8 @@ export default {
   border: 1px solid #bfbfbf;
   border-radius: 2px;
   background-color: #f0efef;
+  .v-tabs-items {
+    background-color: transparent !important;
+  }
 }
 </style>
